e97b20299b0d95b3ac716997eb0ac372ae44d5ce
Kyu Lee <kwooler@gmail.com>
2023-04-15 17:48:27 -0700

    Q42

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ def main():
     ##################################################
     # Code your program here
     ##################################################
-    inputval = input()
-    shortest = longest = inputval
-    minlen = maxlen = len(shortest)
-    while inputval != 'stop' and inputval != 'STOP':
-        if minlen > len(inputval):
-            minlen = len(inputval)
-            shortest = inputval
-        if maxlen < len(inputval):
-            maxlen = len(inputval)
-            longest = inputval
-        inputval = input()
 
     print(longest)
     print(shortest)
